4ebb6780b34f2d38f989e9ced61aa2e5416c4029
Zeyad El-Halawany <whatelseisthere.5@gmail.com>
2022-07-15 14:27:56 +0200

    changes

--- crud.py
+++ crud.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def index():
-    return "<h1> Hello!! </h1>"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
